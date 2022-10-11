4a95c2277b846ccc060ead66adc8d14da2cb9d70
sachingupta63 <sachingupta.6390@gmail.com>
2022-10-11 12:29:24 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/contesto/Activities/MainActivity2.java
@@ -0,0 +1,16 @@
+package com.example.contesto.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.contesto.R;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
---
+++ app/src/main/java/com/example/contesto/Activities/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.example.contesto.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.contesto.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
